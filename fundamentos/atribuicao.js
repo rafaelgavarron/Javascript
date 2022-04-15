2cb849fd62d9e6d01c1d6f0aee6b42a873c580be
Wydune <rafael.gavarron@gmail.com>
2022-04-15 14:59:39 -0300

    Atribuicao

---
+++ fundamentos/atribuicao.js
@@ -0,0 +1,21 @@
+const a = 7; // está atribuindo
+let b = 3;
+
+b += a; //b = b + a
+console.log(b);
+//10
+
+b -= a; //b = b - a
+console.log(b);
+//3
+
+b *= 2; //b = b * 2
+console.log(b);
+//6
+
+b /= 2; // b = b / 2
+console.log(b);
+//3
+
+b %= 2; //b = b % 2 (modular) (se b for par vai retornar 0 se impar 1)
+console.log(b);
