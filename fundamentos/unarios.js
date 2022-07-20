bb32f543ffc089f595fbd44ff3e4bfb1f08609f4
Wydune <rafael.gavarron@gmail.com>
2022-07-20 12:12:14 -0300

    Operadores unários

---
+++ fundamentos/unarios.js
@@ -0,0 +1,13 @@
+let num1 = 1;
+let num2 = 2;
+
+num1++; // acrescenta 1 ao valor da variavel
+console.log(num1);
+--num1; // diminui 1 ao valor da variavel
+console.log(num1);
+
+console.log(++num1 === num2--);
+// deu true pq o num2 decrementou dps da comparação
+console.log(num1 === num2);
+
+// MANTENHA O CODIGO SIMPLES
