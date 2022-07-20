6ef99177fb4c8216593ad9c5eb30f57326bde114
Wydune <rafael.gavarron@gmail.com>
2022-07-20 13:17:21 -0300

    Operadores ternários

---
+++ fundamentos/ternários.js
@@ -0,0 +1,5 @@
+const resultado = (nota) =>
+  nota >= 7 ? /*verdadeiro ou falso*/ 'aprovado' : 'reprovado';
+//primeira vddeiro dois pontos falso
+console.log(resultado(7.1));
+console.log(resultado(6.1));
