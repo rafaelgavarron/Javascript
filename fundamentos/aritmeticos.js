2ac43e9ac07067d50260a5b06d7585ba2205021c
Wydune <rafael.gavarron@gmail.com>
2022-07-19 21:08:16 -0300

    adição do aritmeticos

---
+++ fundamentos/aritmeticos.js
@@ -0,0 +1,20 @@
+const [a, b, c, d] = [3, 5, 1, 15];
+
+const soma = a + b + c + d;
+const subtracao = d - b;
+const multiplicacao = a * b;
+const divisao = d / a;
+const modulo = a % 2; //ele pega o resto da divisao pra printar no console
+
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(modulo);
+
+console.log(soma, subtracao, multiplicacao, -divisao, modulo);
+// "-variavel" deixa a variavel negativa (ou positiva dependendo do sinal)
+
+const asdf = -divisao + 5;
+
+console.log(asdf); // retorna 0
