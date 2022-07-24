7dbc216f536c79a95a80c1c0b5728abd427df2bb
Wydune <rafael.gavarron@gmail.com>
2022-07-24 11:00:30 -0300

    explicações das aulas 1

--- fundamentos/array.js
+++ fundamentos/array.js
@@ -1,16 +1,16 @@
 const valores = [7.7, 8.9, 6.3, 9.2];
 
-console.log(valores[0], valores[3]);
+console.log(valores[0], valores[3]); // ele sempre começa a contar do 0
 console.log(valores[4]);
 
-valores[4] = 10;
+valores[4] = 10; // adiciona o valor 10 ao array 4
 console.log(valores); // printa os valores no total
 console.log(valores.length); // mostra quantos valores tem o array
 
-valores.push({id:3}, false, null, 'teste');
+valores.push({ id: 3 }, false, null, 'teste');
 console.log(valores);
 
 console.log(valores.pop());
-delete valores [0];
-console.log(valores)
-console.log(typeof valores)
+delete valores[0];
+console.log(valores);
+console.log(typeof valores);
