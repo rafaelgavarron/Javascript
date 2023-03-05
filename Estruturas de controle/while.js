be840f786e6d5ef87748008705fc959f7555cf5c
Rafael <rafael.gavarron@gmail.com>
2023-03-05 10:34:47 -0300

    Adição de comentários

--- Estruturas de controle/while.js
+++ Estruturas de controle/while.js
@@ -7,7 +7,7 @@ let opcao = 0;
 
 while (opcao != -1) {
   /*se for falso ele nem entra*/ opcao = getInteiroAleatorioEntre(-1, 10);
-  console.log(`Opção escolhida foi ${opcao}`);
+  console.log(`Opção escolhida foi ${opcao}`); //${alguma coisa} é usado pra sair da string
 }
 
 console.log('até a Próxima!');
